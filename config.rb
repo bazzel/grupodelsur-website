7da70fbb69e16c1e8663dc2e8828c374be2e8d63
Patrick Baselier <patrick.baselier@gmail.com>
2016-01-04 07:02:27 +0100

    Use relative path

--- config.rb
+++ config.rb
@@ -54,6 +54,7 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :partials_dir, 'partials'
+set :relative_links, true
 
 # Build-specific configuration
 configure :build do
