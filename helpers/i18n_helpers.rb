1b94d7779d32d0d29b0fd98fac9c9311135828ec
Patrick Baselier <patrick.baselier@gmail.com>
2016-03-02 10:57:35 +0100

    Add locale links

--- helpers/i18n_helpers.rb
+++ helpers/i18n_helpers.rb
@@ -13,4 +13,30 @@ module I18nHelpers
       "/#{I18n.locale}/#{t(path, scope: :paths)}"
     end
   end
+
+  def locale_links
+    return unless current_page.locale_root_path
+
+    #other_langs = langs.reject { |l| l == I18n.locale }
+
+    content = langs.reduce('') do |text, locale|
+      text << content_tag(:li, locale_link(locale, current_page))
+    end
+
+    content_tag(:ul, content, class: 'locale-links')
+  end
+
+  def locale_link(locale, current_page)
+    classname = "flag-icon-#{locale}"
+
+    if locale == I18n.locale
+      content_tag(:span, locale, class: [classname, 'active'].join(' '))
+    else
+      I18n.with_locale(locale) do
+        path = File.basename(current_page.locale_root_path, current_page.ext)
+        link_to(locale, local_path(path), class: classname)
+      end
+    end
+
+  end
 end
